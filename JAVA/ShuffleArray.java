bc77cfd2cd626670d706f00c0bc304d30890a1f4
Vishal gour <vishalgour0170@gmail.com>
2023-10-13 00:11:58 +0530

    Update ShuffleArray.java

--- JAVA/ShuffleArray.java
+++ JAVA/ShuffleArray.java
@@ -1,21 +1,21 @@
-import java.util.*;
+import java.util.Random;
 
-// i will write the logic in comments
 public class ShuffleArray {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7};
+        shuffleArray(arr);
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
 
-	public static void main(String[] args) {
-
-		        Integer[] arr = {1, 2, 3, 4, 5, 6, 7};// declare array withh 7 elements 1-7.
-		        
-		        List<Integer> list = Arrays.asList(arr); // declare a list using collection framework
-		        // pass the array into list using asList method.
-		        Collections.shuffle(list);  //shuffle method is used for shuffling the elements in list
-		        
-		        System.out.println(Arrays.toString(list.toArray())); 
-		        // to string method which is declared in object class is called to print the Array object as string 
-		    
-
-
-	}
-
+    public static void shuffleArray(int[] arr) {
+        Random rand = new Random();
+        for (int i = arr.length - 1; i > 0; i--) {
+            int j = rand.nextInt(i + 1);
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
 }
